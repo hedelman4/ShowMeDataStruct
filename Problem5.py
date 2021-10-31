f859727fa4c49e467711d0d35d4045ee71181cf1
Harry <hte488@gmail.com>
2021-10-31 12:01:32 -0400

    Problem5 complete

---
+++ Problem5.py
@@ -0,0 +1,55 @@
+import hashlib
+import time
+from datetime import datetime
+
+class Block:
+
+    def __init__(self, timestamp, data, previous_hash):
+      self.timestamp = timestamp
+      self.data = data
+      self.previous_hash = previous_hash
+      self.hash = self.calc_hash(data)
+
+    def calc_hash(self, data):
+          sha = hashlib.sha256()
+          hash_str = data.encode('utf-8')
+          sha.update(hash_str)
+          return sha.hexdigest()
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, value):
+        if self.head is None:
+            self.head = Node(value)
+            return
+
+        # Move to the tail (the last node)
+        node = self.head
+        while node.next:
+            node = node.next
+
+        node.next = Node(value)
+        return
+
+linked_list = LinkedList()
+
+Block0 = Block(datetime.fromtimestamp(time.time()),'Block 0',linked_list.head)
+linked_list.append(Block0)
+
+Block1 = Block(datetime.fromtimestamp(time.time()),'Block 1',linked_list.head)
+linked_list.append(Block1)
+
+Block2 = Block(datetime.fromtimestamp(time.time()),'Block 2',linked_list.head)
+linked_list.append(Block2)
+
+node = linked_list.head
+while node:
+    print(node.value,node.value.data,node.value.previous_hash,node.value.hash)
+    node = node.next
