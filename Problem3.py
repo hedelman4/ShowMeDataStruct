9da50d7d30dbb0184524b6365091eb8973841cbf
Harry <hte488@gmail.com>
2021-11-02 22:32:21 -0400

    Third revised project submission

--- Problem3.py
+++ Problem3.py
@@ -185,3 +185,21 @@ if __name__ == "__main__":
     print ("The content of the encoded data is: {}\n".format(decoded_data))
 
     codeDict.clear()
+
+    #Test Case 3
+    a_great_sentence = " "
+
+    print ("The size of the data is: {}\n".format(sys.getsizeof(a_great_sentence)))
+    print ("The content of the data is: {}\n".format(a_great_sentence))
+
+    encoded_data = huffman_encoding(a_great_sentence)
+
+    print ("The size of the encoded data is: {}\n".format(sys.getsizeof(int(encoded_data, base=2))))
+    print ("The content of the encoded data is: {}\n".format(encoded_data))
+
+    decoded_data = huffman_decoding(encoded_data)
+
+    print ("The size of the decoded data is: {}\n".format(sys.getsizeof(decoded_data)))
+    print ("The content of the encoded data is: {}\n".format(decoded_data))
+
+    codeDict.clear()
